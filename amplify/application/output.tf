b36c96ad131349cd0801eb622ab19de622d72487
jamie <j2clark@gmail.com>
2024-06-24 18:08:57 -0700

    add outputs

---
+++ amplify/application/output.tf
@@ -0,0 +1,4 @@
+output "API_ID" {
+  value = aws_api_gateway_rest_api.restapi.id
+}
+
--- amplify/client/aws-exports.js
+++ amplify/client/aws-exports.js
@@ -1,3 +1,12 @@
 {
+    apiRegion = 'us-west-1',
+    apiHost = '',
 
+    userPoolRegion = 'us-west-1',
+    userPoolId = 'us-west-1_K7vLvqWfz',
+    userPoolHost = 'cognito-idp.'+userPoolRegion+'.amazonaws.com',
+    userPoolWebClientId = "24u1kngpf6g138gb6hf18c21rn",
+
+    identityPoolRegion = 'us-west-1',
+    identityPoolId = 'us-west-1:18670b9a-7b6a-4c47-b658-fdae4d2adc3e'
 }
